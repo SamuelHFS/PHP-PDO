bdaa23c674a42ac07698780a72d17c8d3dbf9c7e
SamuelHFS <samuel.jpg1@gmail.com>
2021-07-26 18:11:28 -0300

    testando em casa

---
+++ PHPpdo/bd/conecta.php
@@ -0,0 +1,18 @@
+<?php
+
+class Conecta{
+
+
+#bd do cadstro pessoal
+    private $url = "localhost:3306";
+    private  $user = "root";
+    private  $password = "senac"; #mudarSenha12345
+    private  $base = "produto1"; #mudarParaPRODUTO1
+
+    
+
+    public function conectadb(){
+        return mysqli_connect($this->url, $this->user, 
+                $this->password, $this->base);
+    }
+}
