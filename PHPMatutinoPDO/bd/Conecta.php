7a4f710075c0142ca3eb0a94b9643ac82bb69ff4
SamuelHFS <samuel.jpg1@gmail.com>
2021-09-14 09:26:20 -0300

    dia 14 sala

--- PHPMatutinoPDO/bd/Conecta.php
+++ PHPMatutinoPDO/bd/Conecta.php
@@ -6,7 +6,7 @@ class Conecta {
         $pdo = null;
         try{
             $pdo = new PDO("mysql:host=localhost;dbname=dbphp01", 
-                    "root", "12345");
+                    "root", "senac");
         } catch (Exception $ex) {
             echo "<script>alert('Erro na conexão com o "
             . "banco de dados.')</script>";
